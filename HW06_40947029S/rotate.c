ae0831a0bdd1e739a91a3e21520cfc0e963ab21f
Vonernue <seanho12345gmail.com>
2020-12-29 01:26:46 +0800

    rotate update

--- HW06_40947029S/rotate.c
+++ HW06_40947029S/rotate.c
@@ -6,7 +6,7 @@
 
 void rotate(double *x,double *y,double theta){
     double postx,posty;
-    theta = M_PI*theta/180;
+    theta = M_PI*-theta/180;
     postx = cos(theta)* *x + sin(theta)*(-1)* *y;
     posty = sin(theta)* *x + cos(theta)* *y;
     *x = postx;
